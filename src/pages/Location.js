5d4c445954b324f758f81913bce6ab21dba33f4a
Shamith R Kumar <shamith2kumar@gmail.com>
2025-04-16 11:54:10 +0530

    Updated Location pages

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -12,7 +12,7 @@ function Location() {
               <Card.Title>Ideal Café – Mangalore</Card.Title>
               <Card.Text>
                 <strong>Address:</strong> MG Road, Mangalore, Karnataka 575001<br />
-                <strong>Hours:</strong> 10:00 AM – 10:00 PM (All days)<br />
+                <strong>Hours:</strong> 10:00 AM – 10:00 PM<br />
                 <strong>Phone:</strong> +91-9876543210
               </Card.Text>
             </Card.Body>
@@ -22,12 +22,14 @@ function Location() {
           <div style={{ height: '300px' }}>
             <iframe
               title="Ideal Café Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!..." // paste actual embed URL here
               width="100%"
               height="100%"
+              frameBorder="0"
               style={{ border: 0 }}
-              allowFullScreen=""
               loading="lazy"
+              allowFullScreen
+              referrerPolicy="no-referrer-when-downgrade"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.700670719253!2d74.83633101479156!3d12.917999690883105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba35ba555555555%3A0xabcdefabcdef1234!2sIdeal%20Cafe!5e0!3m2!1sen!2sin!4v1612345678901"
             ></iframe>
           </div>
         </Col>
